d96d459c1eb7f5bd45f6d80b80bb8f7fa1798b66
AnaisOlivares <anaisolivareshilario@gmail.com>
2017-11-09 09:00:28 -0500

    añadiendo avance

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 window.onload = function () {
   var board = document.querySelector('.board-js');
-   board.addEventListener('dblclick',function(event){
+   /*board.addEventListener('dblclick',function(event){
      event.target.style.backgroundColor = 'rgba(90, 242, 251, 0.87)';
      event.target.style.border = '1px solid yellow';
      event.target.textContent = "X"
@@ -8,9 +8,20 @@ window.onload = function () {
    });
     board.addEventListener('click',function(event){
        event.target.style.backgroundColor = 'yellow';
-        event.target.textContent = "O" /*puede ser innerHTML" TAMBIÉN*/
+        event.target.textContent = "O" /*puede ser innerHTML" TAMBIÉN
    });
-};
+};*/
 
- var centinel = true;
- function 
+board.addEventListener("click", addX);
+ };
+var centinel = false;
+
+function addX (event){
+ if (centinel) {
+ event.target.textContent ="X";
+ centinel = false;
+} else {
+   event.target.textContent ="O";
+   centinel = true;
+}
+}
